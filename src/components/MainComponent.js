ea683c276aa713e1a65ed1b02e06c7cf5f671634
Sreehari <sreeharichunkz@gmail.com>
2020-10-28 11:07:46 +0530

    server_update

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,7 +6,7 @@ import Menu from './MenuComponents2';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import DishDetail from './DetailDish';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
@@ -23,11 +23,12 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-   fetchDishes: () => { dispatch(fetchDishes())},
-    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
-  });
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos())
+});
 
 
 class Main extends Component  {
@@ -37,6 +38,8 @@ class Main extends Component  {
 
 componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
   render(){
@@ -44,22 +47,25 @@ componentDidMount() {
   const HomePage =() =>{
       return(
         <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-            dishesLoading={this.props.dishes.isLoading}
-            dishesErrMess={this.props.dishes.errMess}
-             promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-             leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-         />
+         dishesLoading={this.props.dishes.isLoading}
+         dishErrMess={this.props.dishes.errMess}
+         promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+         promosLoading={this.props.promotions.isLoading}
+         promosErrMess={this.props.promotions.errMess}
+         leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+     />
       );
     }
 
     const DishWithId = ({match}) => {
         return(
           <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-          isLoading={this.props.dishes.isLoading}
-          errMess={this.props.dishes.errMess}
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-          addComment={this.props.addComment}
-    />
+              isLoading={this.props.dishes.isLoading}
+              errMess={this.props.dishes.errMess}
+              comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+              commentsErrMess={this.props.comments.errMess}
+              addComment={this.props.addComment}
+            />
         );
       };
 
