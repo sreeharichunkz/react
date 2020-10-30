1ed49ca383d9db9347d9eadd35e3a3f33860cc16
Sreehari <sreeharichunkz@gmail.com>
2020-10-30 17:26:37 +0530

    all errors solved

--- src/components/DetailDish.js
+++ src/components/DetailDish.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import {Control, Errors, LocalForm} from "react-redux-form";
 import {Loading} from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components' ;
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -101,6 +102,10 @@ class CommentForm extends Component{
 function RenderDish({dish}){
     if(dish != null)
         return (
+          <FadeTransform in
+              transformProps={{
+                exitTransform:'scale(0.5) translateY(-50%)'
+              }}>
             <Card>
                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
@@ -110,6 +115,7 @@ function RenderDish({dish}){
                     </CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         );
     else
         return (
@@ -124,9 +130,11 @@ function RenderComments({comments, postComment,dishId}){
                 <h4>Comments</h4>
             </div>
             <ul className="list-unstyled">
+            <stagger in>
                 {
                     comments.map((comment, i) =>{
                         return(
+                          <Fade in>
                             <li key={comment.id}>
                                 <p>
                                     {comment.comment}
@@ -137,9 +145,11 @@ function RenderComments({comments, postComment,dishId}){
                                     .format(new Date(Date.parse(comment.date)))}
                                 </p>
                             </li>
-                        )
+                            </Fade>
+                        );
                     })
                 }
+                </stagger>
             </ul>
             <CommentForm dishId={dishId} postComment={postComment}/>
         </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
-import {Loading} from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform } from "react-animation-components";
 
-function RenderCard({item, isLoading,errMess}){
-  if(isLoading){
-    return(
-      <Loading />
-    );
-  }
-  else if(errMess){
-    return(
-      <h4>{errMess}</h4>
-    );
-  }
-  else{
-  return(
-    <Card>
-        <CardImg src={baseUrl + item.image} alt={item.name} />
-        <CardBody>
+function RenderCard({ item, isLoading, errMess }) {
+  if (isLoading) {
+    return <Loading />;
+  } else if (errMess) {
+    return <h4>{errMess}</h4>;
+  } else
+    return (
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: "scale(0.5) translateY(-50%)"
+        }}
+      >
+        <Card>
+          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardBody>
             <CardTitle>{item.name}</CardTitle>
-            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-            <CardText>{item.designation}</CardText>
-        </CardBody>
-    </Card>
-
-  );
-}}
+            {item.designation ? (
+              <CardSubtitle>{item.designation}</CardSubtitle>
+            ) : null}
+            <CardText>{item.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
+    );
+}
 
 function Home(props) {
-  return(
+  return (
     <div className="container">
-       <div classsName="row align-item-start">
-
-          <div className="col-12 col-md m-1">
-              <RenderCard item={props.dish}
-               isLoading={props.dishesLoading}
-               errMess={props.dishesErrMess}/>
-          </div>
-
-          <div className="col-12 col-md m-1">
-              <RenderCard item={props.promotion} 
-              isLoading={props.promosLoading}
-              errMess={props.promosErrMess} />
-          </div>
-
-          <div className="col-12 col-md m-1">
-              <RenderCard item={props.leader}/>
-          </div>
-
-
-       </div>
-
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard
+            item={props.dish}
+            isLoading={props.dishesLoading}
+            errMess={props.dishErrMess}
+          />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard
+            item={props.promotion}
+            isLoading={props.promoLoading}
+            errMess={props.promoErrMess}
+          />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard
+            item={props.leader}
+            isLoading={props.leaderLoading}
+            errMess={props.leaderErrMess}
+          />
+        </div>
+      </div>
     </div>
   );
 }
